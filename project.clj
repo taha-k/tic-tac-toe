d57d2f8e973770b85867ac7445adac03654cffd6
tahakachwala <taha.kachwala@zervant.com>
2019-12-02 19:07:25 +0200

    add .gitignore
    INIT 3

--- project.clj
+++ project.clj
@@ -3,7 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [environ "1.1.0"]
+                 [compojure "1.6.1"]
+                 [http-kit "2.3.0"]
+                 [ring/ring-defaults "0.3.2"]
+                 [metosin/ring-http-response "0.9.0"]
+                 [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot tic-tac-toe.core
   :target-path "target/%s"
+  :plugins [[lein-environ "1.1.0"]]
   :profiles {:uberjar {:aot :all}})
